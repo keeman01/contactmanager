20f286d499daa3124644378c9653c8716b99d51f
reactor <kmnparts1@gmail.com>
2018-10-30 23:29:09 +0200

    Updates

---
+++ precache-manifest.641c8109016460ca7f5b5a615ab146d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "72aa82900d090390d8dc",
+    "url": "/contactmanager/static/js/runtime~main.72aa8290.js"
+  },
+  {
+    "revision": "790935139d03dff8d96a",
+    "url": "/contactmanager/static/js/main.79093513.chunk.js"
+  },
+  {
+    "revision": "c031e0138ac7cdbdb6d5",
+    "url": "/contactmanager/static/js/1.c031e013.chunk.js"
+  },
+  {
+    "revision": "790935139d03dff8d96a",
+    "url": "/contactmanager/static/css/main.5bf5a46e.chunk.css"
+  },
+  {
+    "revision": "c031e0138ac7cdbdb6d5",
+    "url": "/contactmanager/static/css/1.7948173c.chunk.css"
+  },
+  {
+    "revision": "b31ee8681f8e2bfdb3ac15ca8cdb70c9",
+    "url": "/contactmanager/index.html"
+  }
+];
